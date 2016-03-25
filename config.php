d9487f0e8326b87e605f222d41a03bbc2110e84d
Nguyen Ba Cuong <cuongnb14@gmail.com>
2016-03-25 15:31:21 +0700

    update config for containerzire

--- config.php
+++ config.php
@@ -1,28 +1,28 @@
 <?php
 // HTTP
-define('HTTP_SERVER', 'http://localhost/opencart/');
+define('HTTP_SERVER', 'http://'.getenv('ENV_HOSTNAME').':'.getenv('ENV_PORT').'/');
 
 // HTTPS
-define('HTTPS_SERVER', 'http://localhost/opencart/');
+define('HTTPS_SERVER', 'http://'.getenv('ENV_HOSTNAME').':'.getenv('ENV_PORT').'/');
 
 // DIR
-define('DIR_APPLICATION', '/opt/lampp/htdocs/opencart/catalog/');
-define('DIR_SYSTEM', '/opt/lampp/htdocs/opencart/system/');
-define('DIR_IMAGE', '/opt/lampp/htdocs/opencart/image/');
-define('DIR_LANGUAGE', '/opt/lampp/htdocs/opencart/catalog/language/');
-define('DIR_TEMPLATE', '/opt/lampp/htdocs/opencart/catalog/view/theme/');
-define('DIR_CONFIG', '/opt/lampp/htdocs/opencart/system/config/');
-define('DIR_CACHE', '/opt/lampp/htdocs/opencart/system/storage/cache/');
-define('DIR_DOWNLOAD', '/opt/lampp/htdocs/opencart/system/storage/download/');
-define('DIR_LOGS', '/opt/lampp/htdocs/opencart/system/storage/logs/');
-define('DIR_MODIFICATION', '/opt/lampp/htdocs/opencart/system/storage/modification/');
-define('DIR_UPLOAD', '/opt/lampp/htdocs/opencart/system/storage/upload/');
+define('DIR_APPLICATION', '/var/www/catalog/');
+define('DIR_SYSTEM', '/var/www/system/');
+define('DIR_IMAGE', '/var/www/image/');
+define('DIR_LANGUAGE', '/var/www/catalog/language/');
+define('DIR_TEMPLATE', '/var/www/catalog/view/theme/');
+define('DIR_CONFIG', '/var/www/system/config/');
+define('DIR_CACHE', '/var/www/system/storage/cache/');
+define('DIR_DOWNLOAD', '/var/www/system/storage/download/');
+define('DIR_LOGS', '/var/www/system/storage/logs/');
+define('DIR_MODIFICATION', '/var/www/system/storage/modification/');
+define('DIR_UPLOAD', '/var/www/system/storage/upload/');
 
 // DB
 define('DB_DRIVER', 'mysqli');
-define('DB_HOSTNAME', 'localhost');
-define('DB_USERNAME', 'root');
-define('DB_PASSWORD', '');
-define('DB_DATABASE', 'opencart');
-define('DB_PORT', '3306');
+define('DB_HOSTNAME', getenv('ENV_DB_HOSTNAME'));
+define('DB_USERNAME', getenv('ENV_DB_USERNAME'));
+define('DB_PASSWORD', getenv('ENV_DB_PASSWORD'));
+define('DB_DATABASE', getenv('ENV_DB_NAME'));
+define('DB_PORT', getenv('ENV_DB_PORT'));
 define('DB_PREFIX', 'oc_');
