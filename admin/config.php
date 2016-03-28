35cd59c3cb505c2f2c756a364873596905b269c9
Nguyen Ba Cuong <cuongnb14@gmail.com>
2016-03-28 22:28:27 +0700

    change path directory for web application

--- admin/config.php
+++ admin/config.php
@@ -8,18 +8,18 @@ define('HTTPS_SERVER', 'http://'.getenv('ENV_HOSTNAME').':'.getenv('ENV_PORT').'
 define('HTTPS_CATALOG', 'http://'.getenv('ENV_HOSTNAME').':'.getenv('ENV_PORT').'/');
 
 // DIR
-define('DIR_APPLICATION', '/var/www/admin/');
-define('DIR_SYSTEM', '/var/www/system/');
-define('DIR_IMAGE', '/var/www/image/');
-define('DIR_LANGUAGE', '/var/www/admin/language/');
-define('DIR_TEMPLATE', '/var/www/admin/view/template/');
-define('DIR_CONFIG', '/var/www/system/config/');
-define('DIR_CACHE', '/var/www/system/storage/cache/');
-define('DIR_DOWNLOAD', '/var/www/system/storage/download/');
-define('DIR_LOGS', '/var/www/system/storage/logs/');
-define('DIR_MODIFICATION', '/var/www/system/storage/modification/');
-define('DIR_UPLOAD', '/var/www/system/storage/upload/');
-define('DIR_CATALOG', '/var/www/catalog/');
+define('DIR_APPLICATION', '/var/www/html/admin/');
+define('DIR_SYSTEM', '/var/www/html/system/');
+define('DIR_IMAGE', '/var/www/html/image/');
+define('DIR_LANGUAGE', '/var/www/html/admin/language/');
+define('DIR_TEMPLATE', '/var/www/html/admin/view/template/');
+define('DIR_CONFIG', '/var/www/html/system/config/');
+define('DIR_CACHE', '/var/www/html/system/storage/cache/');
+define('DIR_DOWNLOAD', '/var/www/html/system/storage/download/');
+define('DIR_LOGS', '/var/www/html/system/storage/logs/');
+define('DIR_MODIFICATION', '/var/www/html/system/storage/modification/');
+define('DIR_UPLOAD', '/var/www/html/system/storage/upload/');
+define('DIR_CATALOG', '/var/www/html/catalog/');
 
 // DB
 define('DB_DRIVER', 'mysqli');
--- config.php
+++ config.php
@@ -6,17 +6,17 @@ define('HTTP_SERVER', 'http://'.getenv('ENV_HOSTNAME').':'.getenv('ENV_PORT').'/
 define('HTTPS_SERVER', 'http://'.getenv('ENV_HOSTNAME').':'.getenv('ENV_PORT').'/');
 
 // DIR
-define('DIR_APPLICATION', '/var/www/catalog/');
-define('DIR_SYSTEM', '/var/www/system/');
-define('DIR_IMAGE', '/var/www/image/');
-define('DIR_LANGUAGE', '/var/www/catalog/language/');
-define('DIR_TEMPLATE', '/var/www/catalog/view/theme/');
-define('DIR_CONFIG', '/var/www/system/config/');
-define('DIR_CACHE', '/var/www/system/storage/cache/');
-define('DIR_DOWNLOAD', '/var/www/system/storage/download/');
-define('DIR_LOGS', '/var/www/system/storage/logs/');
-define('DIR_MODIFICATION', '/var/www/system/storage/modification/');
-define('DIR_UPLOAD', '/var/www/system/storage/upload/');
+define('DIR_APPLICATION', '/var/www/html/catalog/');
+define('DIR_SYSTEM', '/var/www/html/system/');
+define('DIR_IMAGE', '/var/www/html/image/');
+define('DIR_LANGUAGE', '/var/www/html/catalog/language/');
+define('DIR_TEMPLATE', '/var/www/html/catalog/view/theme/');
+define('DIR_CONFIG', '/var/www/html/system/config/');
+define('DIR_CACHE', '/var/www/html/system/storage/cache/');
+define('DIR_DOWNLOAD', '/var/www/html/system/storage/download/');
+define('DIR_LOGS', '/var/www/html/system/storage/logs/');
+define('DIR_MODIFICATION', '/var/www/html/system/storage/modification/');
+define('DIR_UPLOAD', '/var/www/html/system/storage/upload/');
 
 // DB
 define('DB_DRIVER', 'mysqli');
